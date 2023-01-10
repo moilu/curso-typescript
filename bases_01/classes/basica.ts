2cf8b049889e13157ced6943fbfc41522a1fb652
Moises Luna <moises.luna@srax.com>
2023-01-10 11:46:14 -0800

    bases: clases new class

--- bases_01/classes/basica.ts
+++ bases_01/classes/basica.ts
@@ -7,9 +7,12 @@
             public team: string, 
             private realName: string
         ){}
+        public bio(): string{
+            return `${this.name} ${this.team}`;
+        }
     }
 
     const hormiga = new Avenger('Antman', 'Capitán', 'Scott Lang');
-    console.log(hormiga);
+    console.log(hormiga.bio());
     console.log(Avenger.avgAge);
 })();
--- bases_01/main.js
+++ bases_01/main.js
@@ -6,10 +6,13 @@
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} ${this.team}`;
+        }
     }
     Avenger.avgAge = 35;
     const hormiga = new Avenger('Antman', 'Capitán', 'Scott Lang');
-    console.log(hormiga);
+    console.log(hormiga.bio());
     console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
