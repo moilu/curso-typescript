ccc383aa620d7be2be99a157ee7797f069584878
moilu <moisesluna0390@gmail.com>
2023-01-12 22:49:15 -0800

    bases: empezado módulo interfaces

---
+++ bases_01/interfaces/basica.ts
@@ -0,0 +1,29 @@
+(()=> {
+
+    interface Hero {
+        name: string,
+        age?: number,
+        powers: string[],
+        getName: () => string
+    }
+
+    let flash: Hero = {
+        name: 'Barry Allen',
+        age: 29,
+        powers: ['Super speed', 'Time travel'],
+        getName() {
+            return this.name;
+        },
+    }
+
+    let superman: Hero = {
+        name: 'Clark Kent',
+        age: 37,
+        powers: ['Super speed', 'Time travel'],
+        getName() {
+            return this.name;
+        },
+    }
+
+
+})()
